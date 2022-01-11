567fd4a6d7354f93d9fe21253908dce10b7be51e
praveenkl3043 <lalpraveenkl@gmail.com>
2022-01-11 15:54:35 +0530

    calculator

--- index.js
+++ index.js
@@ -1,88 +1,39 @@
-let employees = [
-    {
-      id: 1,
-      first_name: "Suresh",
-      last_name: "Kumar",
-      email: "suresh@gmail.com",
-      gender: "Male",
-      ip_address: "160.192.178.21",
-    },
-  
-    {
-      id: 2,
-      first_name: "Sourav",
-      last_name: "Das",
-      email: "sourav@gmail.com",
-      gender: "Male",
-      ip_address: "160.182.178.21",
-    },
-  
-    {
-      id: 3,
-      first_name: "John",
-      last_name: "paul",
-      email: "john@gmail.com",
-      gender: "Male",
-      ip_address: "160.192.238.21",
-    },
-    {
-      id: 4,
-      first_name: "Pavan",
-      last_name: "Sai",
-      email: "pavan@gmail.com",
-      gender: "Male",
-      ip_address: "160.192.278.21",
-    },
-    {
-      id: 5,
-      first_name: "Sangeetha",
-      last_name: "Das",
-      email: "sangeetha@gmail.com",
-      gender: "Female",
-      ip_address: "160.162.178.21",
-    },
-    {
-      id: 6,
-      first_name: "Tushar",
-      last_name: "Vinocha",
-      email: "tushar@gmail.com",
-      gender: "Male",
-      ip_address: "160.092.178.21",
-    },
-    {
-      id: 7,
-      first_name: "Jenny",
-      last_name: "Paul",
-      email: "jenny@gmail.com",
-      gender: "Female",
-      ip_address: "160.192.178.21",
-    },
-  
-    {
-      id: 8,
-      first_name: "Jhansi",
-      last_name: "P",
-      email: "jhansi@gmail.com",
-      gender: "Female",
-      ip_address: "160.192.78.11",
-    },
-    {
-      id: 9,
-      first_name: "Lavish",
-      last_name: "Jain",
-      email: "lavish@gmail.com",
-      gender: "Male",
-      ip_address: "170.192.178.21",
-    },
-  
-    {
-      id: 10,
-      first_name: "Namrata",
-      last_name: "P",
-      email: "Namrata@gmail.com",
-      gender: "Female",
-      ip_address: "160.192.175.21",
-    },
-  ];
+function add () {
+  var num1=document.getElementById("first").value;
+  var num2=document.getElementById("second").value;
+  result=(parseInt(num1) + parseInt(num2))
+  console.log(result);
 
-  let allempbutton
+  document.getElementsByClassName("output")[0].innerHTML=
+  "Ansewr  is"+" "+ result;
+}
+
+function sub () {
+  var num1=document.getElementById("first").value;
+  var num2=document.getElementById("second").value;
+  result=(parseInt(num1) - parseInt(num2))
+  console.log(result);
+
+  document.getElementsByClassName("output")[0].innerHTML=
+  "Ansewr  is"+" "+ result;
+}
+
+function mul () {
+  var num1=document.getElementById("first").value;
+  var num2=document.getElementById("second").value;
+  result=(parseInt(num1) * parseInt(num2))
+  console.log(result);
+
+  document.getElementsByClassName("output")[0].innerHTML=
+  "Ansewr  is"+" " +result;
+}
+
+function div () {
+  var num1=document.getElementById("first").value;
+  var num2=document.getElementById("second").value;
+  result=(parseInt(num1) / parseInt(num2))
+  console.log(result);
+
+  document.getElementsByClassName("output")[0].innerHTML=
+  "Ansewr  is"+" "+ result;
+}
