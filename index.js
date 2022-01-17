30ff36b41c17bd9a473677a6c2d27463e54c207f
praveenkl3043 <lalpraveenkl@gmail.com>
2022-01-17 22:21:59 +0530

    calci

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-let y=Math.floor(Math.random()* 50 + 1);
-console.log(y);
-var guess=1;
-document.getElementById("submitguess").onclick = function() {
-  let x=document.getElementById("guessfield").value;
-  if(x == y) {
-    alert("its right" )
-  }else if(x >y){
-    guess++;
-    alert("Enter smaller number" );
-  }else{
-    guess++;
-    alert("Enter larger number");
-  }
-}
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
