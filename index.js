4ab346faacc450f18372904afe867cced8b72cb4
praveenkl3043 <lalpraveenkl@gmail.com>
2022-01-22 19:10:02 +0530

    api1

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
-let countdown = document.querySelector(".countdown");
-let time =10;
+const btn=document.getElementsByClassName("enter");
+const result=document.getElementsByClassName("data");
 
-countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= time--;
-setTimeout(() => {
-    countdown.innerText= "Happy independence Day"
+const url="https://www.breakingbadapi.com/api/quote/random";
 
-}, 1000);    
-}, 1000);
-}, 1000);
-}, 1000);
-}, 1000);
-}, 1000);
-}, 1000);
-}, 1000);
-}, 1000);
-}, 1000);
+btn[0].addEventListener("click",async function (){
+
+
+   result[0].innerHTML="loading...";
+    try{
+        let res= await fetch(url);
+        let objet=await res.json();
+        //console.log(objet)
+        result[0].innerHTML=`
+        <h2>Author name:<span>${objet[0].author}<span></h2>
+        <h3>Quote:<span>${objet[0].quote}</span></h3>
+        <h4>series:<span>${objet[0].series}</span></h4>
+        `
+    }catch (error){
+        console.log(error);
+        
+    }
+});
